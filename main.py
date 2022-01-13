9bc9e8e0eab410ae3369e558e11423bab55d4b04
Victor <victorg17@live.com>
2022-01-13 18:33:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Programa para calcular o valor/hora de uma pessoa que recebe mensalmente.
+''' Passos
+Receber o valor do salário do usuário:
+Receber as horas trabalhadas do usuário:
+Dividir o salário por horas trabalhadas:
+Imprimir o valor/hora para o usuário: 
+'''
+
+salario_mensal = input('Qual é seu salário mensal? ')
+horas_trabalhadas = input('Quantas horas você trabalha por mês? ')
+valor_por_hora = (int (salario_mensal) / int (horas_trabalhadas))
+print("O valor por hora que você recebe é %s " % (valor_por_hora))
